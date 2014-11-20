54f1999eadc4c914be4f6fe443b56e88e1127062
Cheney Tsai <cheneytsai@cheneytsai-macbookpro.roam.corp.google.com>
2014-11-20 16:36:57 -0500

    initial commit

--- middlewares/chat_backend.rb
+++ middlewares/chat_backend.rb
@@ -9,17 +9,26 @@ module ChatDemo
     KEEPALIVE_TIME = 15 # in seconds
     CHANNEL        = "chat-demo"
 
+    
+
     def initialize(app)
       @app     = app
       @clients = []
       uri = URI.parse(ENV["REDISCLOUD_URL"])
       @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
       Thread.new do
-        redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-        redis_sub.subscribe(CHANNEL) do |on|
-          on.message do |channel, msg|
-            @clients.each {|ws| ws.send(msg) }
-          end
+        update_count = 1
+        p 'start listening thread'
+        while true do
+            #p 'send update with 100ms pause'
+            
+
+            #TODO: Grab Flow Sensor Reading Here
+            lastline = `tail -1 SENSOR_DATA.txt`
+
+            @clients.each {|ws| ws.send('{"handle":"Update #' + update_count.to_s + '   ","text":"Rate = ' + lastline.to_s + ' "}') }
+            update_count+=1
+            sleep 0.1
         end
       end
     end
@@ -28,13 +37,21 @@ module ChatDemo
       if Faye::WebSocket.websocket?(env)
         ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
         ws.on :open do |event|
+          p 'OPEN'
           p [:open, ws.object_id]
           @clients << ws
+          p @clients
+          p 'done open'
         end
 
         ws.on :message do |event|
+          p 'GETTING A MESSAGE'
           p [:message, event.data]
-          @redis.publish(CHANNEL, sanitize(event.data))
+
+          @clients.each {|ws| ws.send(event.data) }
+
+          #@redis.publish(CHANNEL, sanitize(event.data))
+          p 'DONE get message'
         end
 
         ws.on :close do |event|
